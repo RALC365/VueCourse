466fd139cbfb1a8a55a7b3dab3b6a4cc8ebaf344
RALC365 <alejandro.carcamo@unitec.edu>
2020-07-25 01:29:50 -0600

    Forma tediosa de comunicacion Hijo -> Padre

--- Component comunication/componentes/hijo.js
+++ Component comunication/componentes/hijo.js
@@ -2,13 +2,23 @@ Vue.component('hijo',{
     template://html
     `
     <div class="p-5 bg-success text-white">
-        <h4>Componente hijo</h4>
-        <h5>{{numero}}</h5>
+        <h4>Componente hijo: {{numero}}</h4>
+        <h4>nombre: {{nombre}}</h4>
         <button class="btn btn-danger" @click="numero++">+</button>
     </div>
     `,
-    props:['numero']
-
+    props:['numero'],
+    data(){
+        return {
+            nombre: "Richardson"
+        }
+    },
+    
+    mounted(){
+        //Para comunicarnos con el padre. Se hace después de que el componente
+        //esta montado
+        this.$emit('nombreHijo', this.nombre);
+    }
 
 
 })
--- Component comunication/componentes/padre.js
+++ Component comunication/componentes/padre.js
@@ -4,13 +4,15 @@ Vue.component('padre',{
     <div class="p-5 bg-dark text-white">
         <h2>Componente padre: {{numeroPadre}}</h2>
         <button class="btn btn-danger" @click="numeroPadre++">+</button>
+        <h2>{{nombrePadre}}</h2>
         <!--Con : comvertimos el componente en bind -->
-        <hijo :numero="numeroPadre"></hijo>
+        <hijo :numero="numeroPadre" @nombreHijo="nombrePadre = $event"></hijo>
     </div>
     `,
     data(){
         return{
-            numeroPadre:0
+            numeroPadre:0,
+            nombrePadre: ""
         }
     }
 
