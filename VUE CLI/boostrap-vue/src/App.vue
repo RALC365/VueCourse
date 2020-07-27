c0e798441db58f478584a14c5028ed5ed5811727
RALC365 <alejandro.carcamo@unitec.edu>
2020-07-27 15:09:47 -0600

    Columnas responsivas boostrap vue

--- VUE CLI/boostrap-vue/src/App.vue
+++ VUE CLI/boostrap-vue/src/App.vue
@@ -6,7 +6,7 @@
       <router-link to="/about">About</router-link>
     </div>
     -->
-    <b-navbar toggleable="lg" type="dark" variant="info"> 
+    <b-navbar toggleable="lg" type="dark" variant="info">
       <b-container>
         <!--Navbar responisve-->
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
@@ -16,16 +16,18 @@
         lo importante es el ID, que esta relacionado con toggle-->
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
-        <b-nav-item :to="{name: 'Home'}" active-class="active" exact>Home</b-nav-item>
-        <b-nav-item :to="{name: 'About'}" active-class="active">About</b-nav-item>
-      </b-navbar-nav>
+            <b-nav-item :to="{name: 'Home'}" active-class="active" exact>Home</b-nav-item>
+            <b-nav-item :to="{name: 'About'}" active-class="active">About</b-nav-item>
+            <b-nav-item :to="{name: 'Grid'}" active-class="active">Grid</b-nav-item>
 
-
+          </b-navbar-nav>
         </b-collapse>
-
       </b-container>
     </b-navbar>
-    <router-view/>
+
+    <b-container>
+      <router-view />
+    </b-container>
   </div>
 </template>
 
---
+++ VUE CLI/boostrap-vue/src/views/Grid.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="mt-5">
+        <b-row>
+            <!--cols: dispositivos pequeños,
+            md: dispositivos mediadnos,
+            lg: dispositivos largos-->
+            <b-col cols="12" md="6" lg="4">
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel quisquam deserunt, voluptate aperiam tempore repudiandae omnis quia numquam, sunt facere repellat illum fugiat fuga inventore distinctio animi provident sapiente ad!
+                </p>
+            </b-col>
+            <b-col cols="12" md="6" lg="8">
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel quisquam deserunt, voluptate aperiam tempore repudiandae omnis quia numquam, sunt facere repellat illum fugiat fuga inventore distinctio animi provident sapiente ad!
+                </p>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Grid'
+    }
+</script>
+
+<style scoped>
+
+</style>
