38aa4a723b94ca52374115a815265975e7ba527f
RALC365 <alejandro.carcamo@unitec.edu>
2020-07-27 14:56:21 -0600

    NavBar Boostrap vue

--- VUE CLI/boostrap-vue/src/App.vue
+++ VUE CLI/boostrap-vue/src/App.vue
@@ -1,13 +1,35 @@
 <template>
   <div id="app">
+    <!--
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    -->
+    <b-navbar toggleable="lg" type="dark" variant="info"> 
+      <b-container>
+        <!--Navbar responisve-->
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+        <!--Todo lo que este acá, va a estar oculto es dispositivos md
+        lo importante es el ID, que esta relacionado con toggle-->
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+        <b-nav-item :to="{name: 'Home'}" active-class="active" exact>Home</b-nav-item>
+        <b-nav-item :to="{name: 'About'}" active-class="active">About</b-nav-item>
+      </b-navbar-nav>
+
+
+        </b-collapse>
+
+      </b-container>
+    </b-navbar>
     <router-view/>
   </div>
 </template>
 
+<!--
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -30,3 +52,4 @@
   color: #42b983;
 }
 </style>
+-->
